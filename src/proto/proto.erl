7791e897daa4e76e52813f84d52c58d6dae8bc07
dominic <389338435@qq.com>
2020-07-21 16:24:23 +0800

    proto

--- src/proto/proto.erl
+++ src/proto/proto.erl
@@ -8,7 +8,7 @@
 -module(proto).
 -author("dominic").
 
--include("test_pb.hrl").
+-include("test_0_pb.hrl").
 
 %% API
 -export([]).
@@ -19,11 +19,11 @@
 
 base_test_() ->
     Msg = #proto_test{str = <<"str">>, ui32 = 123, rp_str = [<<"中文"/utf8>>, <<"abc123">>], rp_ui32 = [123, 456]},
-    B = (catch test_pb:encode_msg(Msg)),
+    B = (catch test_0_pb:encode_msg(Msg)),
     [
         ?_assert(is_binary(B)),
-        ?_assertEqual(Msg, test_pb:decode_msg(B, proto_test)),
-        ?_test(enif_protobuf:load_cache(test_pb:get_msg_defs())),
+        ?_assertEqual(Msg, test_0_pb:decode_msg(B, proto_test)),
+        ?_test(enif_protobuf:load_cache(test_0_pb:get_msg_defs())),
         ?_assertEqual(Msg, enif_protobuf:decode(B, proto_test)),
         ?_assertEqual(B, enif_protobuf:encode(Msg))
     ].
