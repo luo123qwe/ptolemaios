5e6ed52afd67cc7dc1d6a0f7d28ce3856af12b94
Dominic <38338435@qq.com>
2020-12-03 14:58:21 +0800

    优化

--- rebar3.cmd
+++ rebar3.cmd
@@ -1,4 +1,11 @@
 @echo off
+rem window编译C需要先设置环境
+rem 安装vs, 把cl.exe和vcvarsall.bat放到环境变量
+rem 改变当前编译环境
+call vcvarsall.bat x64
+rem 设置编码
+chcp 65001
+
 setlocal
 set rebarscript=%~f0
 escript.exe "%rebarscript:.cmd=%" %*
