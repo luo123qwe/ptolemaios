cb419bc72f14e84944138a2cabd94917b3816fc4
Dominic <389338435@qq.com>
2021-01-11 21:02:10 +0800

    大改

--- xlsx2erl/src/xlsx2erl_reader.erl
+++ xlsx2erl/src/xlsx2erl_reader.erl
@@ -12,7 +12,7 @@
 -include("xlsx2erl.hrl").
 
 %% API
--export([sheets_without_data/1, sheets_with_data/1, sheets_with_data/3]).
+-export([sheets_without_data/1, sheets_with_data/1, sheets_with_data_1/3, to_form/1, convert_xml_string/1, sheets_with_data/3]).
 
 -spec sheets_without_data(file:filename()) -> [#xlsx2erl_sheet{}].
 sheets_without_data(Filename) ->
@@ -33,12 +33,12 @@ sheets_without_data(Filename) ->
         FullName = attr_find("name", SheetAttr),
         case string:split(FullName, ?XLSX2ERL_SPLIT) of
             [_, Name] ->
-                RId = attr_find("id", SheetAttr),
+                RId = attr_find("r:id", SheetAttr),
                 [#xlsx2erl_sheet{
                     name = list_to_atom(Name), full_name = FullName,
                     workbook_name = list_to_atom(WorkbookName), filename = Filename,
                     module = ?XLSX2ERL_ERL_NAME2(WorkbookName, Name),
-                    id = RId,% 因为先进行了正则匹配, "r:id"在这里会变成id
+                    id = RId,
                     taget = "xl/" ++ maps:get(RId, RIdMapping)
                 } | Acc];
             _ ->% 名字格式不对
@@ -49,8 +49,27 @@ sheets_without_data(Filename) ->
 sheets_with_data(Filename) ->
     sheets_with_data(Filename, [{Key, 99999999} || Key <- ?XLSX2ERL_KEY_LIST], 99999999).
 
-sheets_with_data([], _, _) -> [];
-sheets_with_data([#xlsx2erl_sheet{filename = Filename, taget = Target} = Sheet | T], KeyTimesList, SearchLimit) ->
+
+sheets_with_data(Filename, KeyTimesList, SearchLimit) ->
+    %% 数量大的话, 解析xml的时候进程会吃满内存, gc不降低
+    %% 继续在那个进程操作很可能爆掉
+    %% 发回来耗时很低, 可以有效降低内存使用
+    Self = self(),
+    {Pid, Ref} = spawn_monitor(fun() ->
+        Self ! {sheets_with_data, sheets_with_data_1(Filename, KeyTimesList, SearchLimit)},
+        io:format("~w~n", [erlang:memory()])
+                               end),
+    receive
+        {sheets_with_data, Data} ->
+            erlang:demonitor(Ref, [flush]),
+            Data;
+        {'DOWN', Ref, Pid, Reason} ->
+            ?XLSX2ERL_ERROR2("sheets_with_data error ~p", [Reason])
+    end.
+
+
+sheets_with_data_1([], _, _) -> [];
+sheets_with_data_1([#xlsx2erl_sheet{filename = Filename, taget = Target} = Sheet | T], KeyTimesList, SearchLimit) ->
     FD = get_fd(Filename),
     {ok, {_, Xml}} = zip:zip_get(Target, FD),
     SheetForm = xml_to_form("worksheet", Xml, Filename),
@@ -59,9 +78,9 @@ sheets_with_data([#xlsx2erl_sheet{filename = Filename, taget = Target} = Sheet |
     %% 构造行数据
     Key2RowListMap = sheets_with_data_key2row_list(SheetDataForm, KeyTimesList, SearchLimit, ShareString, #{}),
     Sheet1 = Sheet#xlsx2erl_sheet{data = Key2RowListMap},
-    [Sheet1 | sheets_with_data(T, KeyTimesList, SearchLimit)];
-sheets_with_data(Filename, KeyTimesList, SearchLimit) ->
-    sheets_with_data(sheets_without_data(Filename), KeyTimesList, SearchLimit).
+    [Sheet1 | sheets_with_data_1(T, KeyTimesList, SearchLimit)];
+sheets_with_data_1(Filename, KeyTimesList, SearchLimit) ->
+    sheets_with_data_1(sheets_without_data(Filename), KeyTimesList, SearchLimit).
 
 sheets_with_data_key2row_list([], _KeyTimesList, _SearchLimit, _ShareString, Key2RowListMap) ->
     maps:map(fun(_K, V) -> lists:reverse(V) end, Key2RowListMap);
@@ -115,27 +134,19 @@ sheets_with_data_key2row_list([{"row", RowAttr, [{"c", CAttrH, CFormH} | RowForm
     sheets_with_data_key2row_list(T, KeyTimesList1, SearchLimit - 1, ShareString, Key2RowListMap1).
 
 get_cell_value(ShareString, CAttr, CForm) ->
-    [V] = form_find("v", CForm, [undefined]),
+    V = form_find("v", CForm, undefined),
     case attr_find("t", CAttr, "str") of
         "s" -> % ShareString
             maps:get(V, ShareString);
         "b" -> % boolean
             ?IF(V == "1", "TRUE", "FALSE");
         "str" ->
-            V
+            convert_xml_string(V)
     end.
 
-xml_to_form(Tag, Xml, Filename) ->
-    %% 先用正则把 namespace 去掉, 否者解析出来的标签会很长, 而且可能会爆内存
-    %% 例如: {http://schemas.microsoft.com/office/spreadsheetml/2009/9/ac}dyDescent
-    %% 效率: 先re + simple_form > 直接simple_form
-    Xml1 = re:replace(Xml, "\sxmlns.*?\".*?\"", "", [global, {return, binary}]),
-    case erlsom:simple_form(Xml1, []) of
-        {ok, {Tag, _Attr, Form}, []} ->
-            Form;
-        Error ->
-            ?XLSX2ERL_ERROR2("xml_to_from ~ts error:~n~p", [Filename, Error])
-    end.
+xml_to_form(Tag, Xml, _Filename) ->
+    [{Tag, _Attr, Form}] = to_form(Xml),
+    Form.
 
 get_fd(Filename) ->
     case get(?PD_XLSX2ERL_ZIP_FD1(Filename)) of
@@ -165,9 +176,9 @@ get_share_string(Filename) ->
                             %% 看别人实现有这种情况
                             RForm = form_find("r", SIForm),
                             {_, _, [Str]} = form_find("t", RForm),
-                            {I + 1, M#{integer_to_list(I) => Str}};
-                        {_, _, [Str]} ->
-                            {I + 1, M#{integer_to_list(I) => Str}}
+                            {I + 1, M#{integer_to_list(I) => convert_xml_string(Str)}};
+                        {_, _, Str} ->
+                            {I + 1, M#{integer_to_list(I) => convert_xml_string(Str)}}
                     end
                             end, {0, #{}}, ShareStringForm),
             put(?PD_XLSX2ERL_SHARE_STRING1(Filename), Map),
@@ -179,7 +190,7 @@ get_share_string(Filename) ->
 
 attr_find(K, L) ->
     case lists:keyfind(K, 1, L) of
-        false -> ?XLSX2ERL_ERROR2("no ~w in ~w", [K, L]);
+        false -> ?XLSX2ERL_ERROR2("no ~s in ~p", [K, L]);
         {_, V} -> V
     end.
 
@@ -191,7 +202,7 @@ attr_find(K, L, Default) ->
 
 form_find(K, L) ->
     case lists:keyfind(K, 1, L) of
-        false -> ?XLSX2ERL_ERROR2("no ~w in ~w", [K, L]);
+        false -> ?XLSX2ERL_ERROR2("no ~s in ~p", [K, L]);
         {_, _Attr, Form} -> Form
     end.
 
@@ -208,4 +219,146 @@ get_column_index(ColumnStr) ->
             {Exp * 26, (Char - $A + 1) * Exp + S}
                     end, {1, 0}, WordStr),
     Sum.
-    
+
+
+%% List遍历的效率 >> Binary
+to_form(Bin) when is_binary(Bin) ->
+    to_form(binary_to_list(Bin));
+to_form(List) ->
+    case List of
+        "<?xml" ++ List1 ->% 去掉xml定义
+            to_form_body(to_form_skip_xml_head(List1), []);
+        _ ->
+            to_form_body(List, [])
+    end.
+
+%% 属性值里的字符串, 防止冲突 xxx="yyy"
+to_form_skip_xml_head([$" | List]) ->
+    List1 = to_form_skip_xml_head_string(List),
+    to_form_skip_xml_head(List1);
+to_form_skip_xml_head([$  | List]) ->
+    to_form_skip_xml_head(List);
+%% 结束 ?>\r\n
+to_form_skip_xml_head([$?, $>, $\r, $\n | List]) ->
+    List;
+%% 其他字符直接跳过就行
+to_form_skip_xml_head([_ | List]) ->
+    to_form_skip_xml_head(List).
+
+to_form_skip_xml_head_string([$\\, $" | List]) ->
+    to_form_skip_xml_head_string(List);
+to_form_skip_xml_head_string([$" | List]) ->
+    List;
+to_form_skip_xml_head_string([_ | List]) ->
+    to_form_skip_xml_head_string(List).
+
+%% <a></a> => {"a", [], []}
+%% <a b="b"></a> => {"a", [{"b", "b"}], []}
+%% <a b="b">c</a> => {"a", [{"b", "b"}], "c"}
+%% <a b="b"><c/></a> => {"a", [{"b", "b"}], [{"c", [], []}]}
+to_form_body([], Acc) ->
+    lists:reverse(Acc);
+%% 开始转换标签 <xxx...
+to_form_body([$< | List], Acc) ->
+    {Element, List1} = to_form_element(List, []),
+    to_form_body(List1, [Element | Acc]).
+
+%% 标签名字收集完成 <xxx attr="attr"...
+to_form_element([$  | List], Tag) ->
+    to_form_element_attr(List, lists:reverse(Tag), [], []);
+%% "空的"标签 <xxx/>
+to_form_element([$/, $> | List], Tag) ->
+    {{lists:reverse(Tag), [], []}, List};
+%% 开始的标签 <xxx>yyyyyy...
+to_form_element([$> | List], Tag) ->
+    {Listody, List1} =
+        case List of
+            %% 标签的内容是标签 <xxx><yyy....
+            [$< | _] -> to_form_element_body_element(List, []);
+            %% 标签的内容是字符串 <xxx>yyy...
+            _ -> to_form_element_body_string(List, [])
+        end,
+    {{lists:reverse(Tag), [], Listody}, List1};
+%% 收集标签名字
+to_form_element([C | List], Tag) ->
+    to_form_element(List, [C | Tag]).
+
+%% 属性名字收集完成 xxx="yyy....
+to_form_element_attr([$=, $" | List], Tag, Attr, AttrList) ->
+    {Value, List1} = to_form_element_attr_value(List, []),
+    to_form_element_attr(List1, Tag, [], [{lists:reverse(Attr), Value} | AttrList]);
+%% 标签结束 <xxx yyy="yyy"/>
+to_form_element_attr([$/, $> | List], Tag, _Attr, AttrList) ->
+    {{Tag, lists:reverse(AttrList), []}, List};
+%% 标签结束 <xxx yyy="yyy">
+to_form_element_attr([$> | List], Tag, _Attr, AttrList) ->
+    {Body, List1} =
+        case List of
+            %% 标签的内容是标签 <xxx><yyy....
+            [$< | _] -> to_form_element_body_element(List, []);
+            %% 标签的内容是字符串 <xxx>yyy...
+            _ -> to_form_element_body_string(List, [])
+        end,
+    {{Tag, lists:reverse(AttrList), Body}, List1};
+%% 属性空格隔开, 忽略即可 <xxx yyy="yyy" zzz="zzz"...
+to_form_element_attr([$  | List], Tag, _Attr, AttrList) ->
+    to_form_element_attr(List, Tag, _Attr, AttrList);
+%% 收集属性名字
+to_form_element_attr([C | List], Tag, Attr, AttrList) ->
+    to_form_element_attr(List, Tag, [C | Attr], AttrList).
+
+%% 去掉前后双引号, 值都是 "\"xxxx\""
+to_form_element_attr_value([$\\, $" | List], Value) ->
+    to_form_element_attr_value(List, [$\\, $" | Value]);
+to_form_element_attr_value([$" | List], Value) ->
+    {lists:reverse(Value), List};
+to_form_element_attr_value([C | List], Value) ->
+    to_form_element_attr_value(List, [C | Value]).
+
+%% 出现结束标签 </xxx...>
+%% 这里不会检查名字
+to_form_element_body_element([$<, $/ | List1], Body) ->
+    {lists:reverse(Body), to_form_skip_tag(List1)};
+%% 出现标签
+to_form_element_body_element([$< | List], Body) ->
+    {Element, List1} = to_form_element(List, []),
+    to_form_element_body_element(List1, [Element | Body]).
+
+%% 字符串内容
+to_form_element_body_string([$<, $/ | List1], Body) ->
+    {lists:reverse(Body), to_form_skip_tag(List1)};
+to_form_element_body_string([C | List], Body) ->
+    to_form_element_body_string(List, [C | Body]).
+
+to_form_skip_tag([$> | List]) -> List;
+to_form_skip_tag([_ | List]) -> to_form_skip_tag(List).
+
+%% 转义字符
+%% &lt; -> <
+%% &gt; -> >
+%% &amp; -> &
+%% &apos; -> '
+%% &quot; -> "
+convert_xml_string(List) ->
+    %% 粗过滤
+    case lists:member($&, List) of
+        true ->
+            convert_xml_string(List, []);
+        _ ->
+            List
+    end.
+
+convert_xml_string([], List) ->
+    lists:reverse(List);
+convert_xml_string([$&, $l, $t, $; | T], List) ->
+    convert_xml_string(T, [$< | List]);
+convert_xml_string([$&, $g, $t, $; | T], List) ->
+    convert_xml_string(T, [$> | List]);
+convert_xml_string([$&, $a, $m, $p, $; | T], List) ->
+    convert_xml_string(T, [$< | List]);
+convert_xml_string([$&, $a, $p, $o, $s, $; | T], List) ->
+    convert_xml_string(T, [$< | List]);
+convert_xml_string([$&, $q, $u, $o, $t, $; | T], List) ->
+    convert_xml_string(T, [$< | List]);
+convert_xml_string([H | T], List) ->
+    convert_xml_string(T, [H | List]).
